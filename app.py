e57992b8973b92530f95aa3590052339fde9c2a6
flunkout-dvlpr <info@juliogonzalez.space>
2022-02-18 18:14:15 -0500

    Create helper functions to read/update json file, integrate pandas to store/manipulate data, update file by writing modified data frame to json

--- app.py
+++ app.py
@@ -1,24 +1,50 @@
 import json
-
+import pandas as pd
 from flask import Flask, jsonify, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+
+JSON_FILE = "starter-data.json"
 
 app = Flask(__name__)
 api = Api(app)
 
-with open("starter-data.json", "r") as fp:
-    employees = json.load(fp)
+parser = reqparse.RequestParser()
+parser.add_argument('employee', type=dict, required=True)
+
+def readFile(JSON_FILE):
+    with open(JSON_FILE, "r") as fp:
+        data = json.load(fp)
+    return data["employees"]
+
+def updateFile(JSON_FILE, data):
+    with open(JSON_FILE, 'w') as fp:
+        json.dump({ "employees": data }, fp)
 
 class Employees(Resource):
 
     def get(self):
-        return employees
+        data = readFile(JSON_FILE)
+        dataFrame = pd.DataFrame.from_dict(data)
+        response = dataFrame.to_dict("records")
+        return response, 200
 
-api.add_resource(Employees, "/")
+    def post(self):
+        data = readFile(JSON_FILE)
+        args = parser.parse_args()
+        body = args["employee"]
 
-# @app.route("/")
-# def index():
-#     return jsonify(employees)
+        #Create datatframe and append new employee object
+        dataFrame = pd.DataFrame.from_dict(data)
+        dataFrame = dataFrame.append(body, ignore_index=True)
+
+        #Write update data to file
+        response = dataFrame.to_dict("records")
+        updateFile(JSON_FILE, response)
+
+        return response, 200
+
+api.add_resource(Employees, "/", "/employees")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
