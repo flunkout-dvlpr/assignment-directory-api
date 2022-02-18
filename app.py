09e7f44346ecaccdaac64417650426c3ea0cc733
flunkout-dvlpr <info@juliogonzalez.space>
2022-02-18 16:17:23 -0500

    Create class for endpoints relating to employees, set up get route to list employees

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 import json
 
 from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 with open("starter-data.json", "r") as fp:
     employees = json.load(fp)
 
-@app.route("/")
-def index():
-    return jsonify(employees)
+class Employees(Resource):
+
+    def get(self):
+        return employees
+
+api.add_resource(Employees, "/")
+
+# @app.route("/")
+# def index():
+#     return jsonify(employees)
 
 if __name__ == "__main__":
     app.run(debug=True)
